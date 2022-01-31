c7b8a0291fbde944d91743f145635eac91fa953e
Ruizheng Xu <48921807+ruizhengxu@users.noreply.github.com>
2022-01-31 15:30:48 +0100

    Add files via upload

---
+++ TP_QT1/mainWindow.py
@@ -0,0 +1,32 @@
+#import sys
+#from PyQt5.QtWidgets import *
+
+
+#class MainWindow(QMainWindow):
+#	
+	##############
+#	def __init__(self):
+#		print("constructeur de la class MainWindow")
+#
+#
+#	###############
+#	def open(self):
+#		print("Open...")
+#	
+#	###############
+#	def save():
+#		print("Save")		
+#
+#	###############
+#	def quit():
+#		print("Quit")		
+
+
+
+def main(args):
+	print("Hello World")
+	
+	
+
+if __name__ == "__main__":
+	main(sys.argv) 

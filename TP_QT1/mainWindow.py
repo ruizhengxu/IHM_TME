ce3c736f27a2907e3bf606889f7bec52f5f21b24
Ruizheng <ruizhengxu@gmail.com>
2022-02-02 18:20:25 +0100

    cut, copy, paste

--- TP_QT1/mainWindow.py
+++ TP_QT1/mainWindow.py
@@ -1,4 +1,6 @@
 import sys, resources
+
+from functools import partial
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
@@ -15,44 +17,87 @@ from PyQt5.QtGui import *
 # Q4: Comment connecter les actions aux slots ?
 #	yourAction.triggered.connect(self.your_function)
 
+HEIGHT = 720
+WIDTH = 1280
+
 class MainWindow(QMainWindow):
     
     def __init__(self):
         # print("constructeur de la class MainWindow")
         super().__init__(None)
+        self.initUI()
+        self.createAction()
+        self.initMenuBar()
+        self.initStatusBar()
+        self.initToolBar()
         self.textEdit = QTextEdit()
         self.setCentralWidget(self.textEdit)
+    
+    def initUI(self):
+        self.setWindowTitle('File Editor')
+        self.center()
+        self.show()
+    
+    def createAction(self):
+        # Create different actions
+        self.openAct = QAction(QIcon(":/images/open.png"), "Open", self)
+        self.openAct.setShortcut(QKeySequence("Ctrl+O"))
+        self.openAct.setToolTip("Open File")
+        self.openAct.setStatusTip("Open a file")
+        self.openAct.triggered.connect(self.openFile) # Connect the action to the slot (function)
+        
+        self.saveAct = QAction(QIcon(":/images/save.png"), "Save", self)
+        self.saveAct.setShortcut(QKeySequence("Ctrl+S"))
+        self.saveAct.setToolTip("Save File")
+        self.saveAct.setStatusTip("Save to file")
+        self.saveAct.triggered.connect(self.saveFile)
+        
+        self.cutAct = QAction(QIcon(":/images/cut.png"), "Cut", self)
+        self.cutAct.setShortcut(QKeySequence("Ctrl+X"))
+        self.cutAct.setToolTip("Cut")
+        self.cutAct.setStatusTip("Cut")
+        self.cutAct.triggered.connect(self.cut)
+        
+        self.copyAct = QAction(QIcon(":/images/copy.png"), "Copy", self)
+        self.copyAct.setShortcut(QKeySequence("Ctrl+C"))
+        self.copyAct.setToolTip("Copy")
+        self.copyAct.setStatusTip("Copy")
+        self.copyAct.triggered.connect(self.copy)
+        
+        self.pasteAct = QAction(QIcon(":/images/paste.png"), "Paste", self)
+        self.pasteAct.setShortcut(QKeySequence("Ctrl+V"))
+        self.pasteAct.setToolTip("Paste")
+        self.pasteAct.setStatusTip("Paste")
+        self.pasteAct.triggered.connect(self.paste)
+        
+        self.quitAct = QAction(QIcon(":/images/quit.png"), "Quit", self)
+        self.quitAct.setShortcut(QKeySequence("Ctrl+Q"))
+        self.quitAct.setToolTip("Quit File")
+        self.quitAct.setStatusTip("Quit Application")
+        self.quitAct.triggered.connect(self.quitFile)
+    
+    def initMenuBar(self):
         self.menuBar = self.menuBar()
         # bar.setNativeMenuBar(False) # D'ont use OS native bar
-        self.statusBar = self.statusBar()
-        
         fileMenu = self.menuBar.addMenu("File")
         
-        # Create different actions
-        fileOpenAct = QAction(QIcon(":/images/open.png"), "Open", self)
-        fileOpenAct.setShortcut(QKeySequence("Ctrl+O"))
-        fileOpenAct.setToolTip("Open File")
-        fileOpenAct.setStatusTip("Open a file")
-        fileOpenAct.triggered.connect(self.openFile) # Connect the action to the slot (function)
-        fileMenu.addAction(fileOpenAct) # Add action to "File" menu
+        fileMenu.addAction(self.openAct) # Add action to "File" menu
+        fileMenu.addAction(self.saveAct)
+        fileMenu.addAction(self.quitAct)
         
-        fileSaveAct = QAction(QIcon(":/images/save.png"), "Save", self)
-        fileSaveAct.setShortcut(QKeySequence("Ctrl+S"))
-        fileSaveAct.setToolTip("Save File")
-        fileSaveAct.setStatusTip("Save to file")
-        fileSaveAct.triggered.connect(self.saveFile)
-        fileMenu.addAction(fileSaveAct)
+    def initStatusBar(self):
+        self.statusBar = self.statusBar()
         
-        fileQuitAct = QAction(QIcon(":/images/quit.png"), "Quit", self)
-        fileQuitAct.setShortcut(QKeySequence("Ctrl+Q"))
-        fileQuitAct.setToolTip("Quit File")
-        fileQuitAct.setStatusTip("Quit Application")
-        fileQuitAct.triggered.connect(self.quitFile)
-        fileMenu.addAction(fileQuitAct)
-        
+    def initToolBar(self):
+        editToolBar = self.addToolBar("Edit")
+        editToolBar.addAction(self.cutAct)
+        editToolBar.addAction(self.copyAct)
+        editToolBar.addAction(self.pasteAct)
+      
     def openFile(self):
         fileName = QFileDialog.getOpenFileName(self, "Open File", "", "Text files (*.txt *.html)")[0]
         self.statusBar.showMessage("Open " + fileName, 6000)
+        self.setWindowTitle(fileName)
         # Read file
         if fileName != "":
             f = open(fileName, "r")
@@ -74,22 +119,67 @@ class MainWindow(QMainWindow):
                 f.write(self.textEdit.toPlainText())
             self.statusBar.showMessage("Saved to " + fileName, 6000)
             f.close()
+    
+    def getSelectTextPos(self):
+        cursor = self.textEdit.textCursor()
+        start = int(cursor.selectionStart())
+        end = int(cursor.selectionEnd())
+        return start, end
+        
+    def cut(self):
+        clipboard = QApplication.clipboard()
+        s, e = self.getSelectTextPos()
+        selectedText = self.textEdit.toPlainText()[s:e]
+        if selectedText != "":
+            clipboard.setText(selectedText)
+            curText = self.textEdit.toPlainText()
+            if self.windowTitle().split(".")[1] == "html":
+                self.textEdit.setHtml(curText[0:s] + curText[e:len(curText)])
+            else:
+                self.textEdit.setPlainText(curText[0:s] + curText[e:len(curText)])
+            self.textEdit.moveCursor(QTextCursor.position(s))
+           
+    def copy(self):
+        clipboard = QApplication.clipboard()
+        s, e = self.getSelectTextPos()
+        if self.windowTitle().split(".")[1] == "html":
+            selectedText = self.textEdit.toHtml()[s:e]
+        else:
+            selectedText = self.textEdit.toPlainText()[s:e]
+        if selectedText != "": clipboard.setText(selectedText)
+    
+    def paste(self):
+        clipboard = QApplication.clipboard()
+        if self.windowTitle().split(".")[1] == "html":
+            self.textEdit.insertHtml(clipboard.text())
+        else:
+            self.textEdit.insertPlainText(clipboard.text())
         
     def quitFile(self):
         response = QMessageBox.question(self, "Confirm exit", 
                                         "Are you sure you want to exit ?")
         if response == QMessageBox.Yes:
             sys.exit()
+        return response
+    
+    def center(self):
+        screen = QApplication.primaryScreen()
+        self.setGeometry(int(screen.size().width()/2)-int(WIDTH/2),
+                         int(screen.size().height()/2)-int(HEIGHT/2), 
+                         WIDTH, HEIGHT)
         
+    def closeEvent(self, event):
+        response = self.quitFile()
+        if response == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
         
 def main(args):
     # print("Hello World")
     app = QApplication(args)
-    
     win = MainWindow()
-    win.show()
-    win.resize(800, 600)
-    
+    win.resize(WIDTH, HEIGHT)
     app.exec()
     
 if __name__ == "__main__":
